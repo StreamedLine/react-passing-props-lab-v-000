275b5d874388d4633052f0b7b6c9025e115c9c91
streamedLine <dovidlwtn@gmail.com>
2018-03-19 23:13:43 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,27 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			// filters: [],
+      		selectedFilter: null
+      	}
+	}
+
+	handleFilterChange = (event)=> {
+   	    console.log('new filter: ', event.target.value);
+   		this.setState({ selectedFilter: event.target.value });
+  	}
+
+	render() {
+		return (
+			<FruitBasket handleChange={this.handleFilterChange.bind(this)} 
+						 selectedFilter={this.state.selectedFilter} />
+		)
+	}	
+} 
 
 export default App;
